60afd994187846ce1606ac2ec2368491f7d3fbcd
Igor Anthony <igor.maciel@sptech.school>
2024-11-04 10:52:10 -0300

    Iniciando o back-end

---
+++ back-end/autolink/src/main/java/com/autolink/AutolinkApplication.java
@@ -0,0 +1,13 @@
+package com.autolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolinkApplication.class, args);
+	}
+
+}
